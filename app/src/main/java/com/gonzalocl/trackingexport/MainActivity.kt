6b6a115bf943945a6953acbd1dba314b8140a759
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-12-22 20:16:16 +0100

    init

---
+++ app/src/main/java/com/gonzalocl/trackingexport/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gonzalocl.trackingexport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
