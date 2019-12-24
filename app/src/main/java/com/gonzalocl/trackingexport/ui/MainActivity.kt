9dad4efa88cc00c8747b08ca7613472724d5a5f2
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-12-24 15:13:10 +0100

    template file and export file

--- app/src/main/java/com/gonzalocl/trackingexport/ui/MainActivity.kt
+++ app/src/main/java/com/gonzalocl/trackingexport/ui/MainActivity.kt
@@ -6,12 +6,18 @@ import android.os.Bundle
 import android.os.Environment
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import com.gonzalocl.trackingexport.R
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.util.stream.Collectors
 
 class MainActivity : AppCompatActivity() {
 
     private val tracksPath = "Tracks"
+    private val tracksExportPath = "TracksExport"
     private val documentsPath = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) Environment.DIRECTORY_DOCUMENTS else "Documents"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +36,51 @@ class MainActivity : AppCompatActivity() {
         val carReturn = trackFiles[trackFiles.size-2]
 
         val trackTitle = findViewById<EditText>(R.id.track_title).text.toString()
+        val trackDate = carGoing.name.split("_")[0]
+
+        val templateInputStream = resources.openRawResource(R.raw.export_file_template)
+        val templateInputStreamReader = InputStreamReader(templateInputStream)
+        val templateBufferedReader = BufferedReader(templateInputStreamReader)
+
+        val templateString = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            templateBufferedReader.lines().collect(Collectors.joining("\n"))
+        } else {
+//            TODO("VERSION.SDK_INT < N")
+            Toast.makeText(this, "NOT IMPLEMENTED", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        templateInputStream.close()
+        templateInputStreamReader.close()
+        templateBufferedReader.close()
+
+        val tracksExportDir = File(Environment.getExternalStoragePublicDirectory(documentsPath), tracksExportPath)
+        tracksExportDir.mkdirs()
+
+        val exportedFile = File(tracksExportDir, "${trackDate}_${trackTitle}.kml")
+        if (exportedFile.exists() || exportedFile.isDirectory) {
+            Toast.makeText(this, "FILE EXISTS", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (!exportedFile.createNewFile()) {
+            Toast.makeText(this, "FAILED TO CREATE FILE", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val exportedFilePrintWriter = PrintWriter(exportedFile)
+        exportedFilePrintWriter.printf(templateString)
+
+        exportedFilePrintWriter.close()
+
+/*        val shareTrack = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_STREAM, exportedFile.toUri().toString())
+//            putExtra(Intent.EXTRA_STREAM, Uri.parse(exportedFile.toUri().toString()))
+//            putExtra(Intent.EXTRA_STREAM, Uri.parse("content://"+exportedFile.absolutePath))
+            type = "application/vnd.google-earth.kml+xml"
+        }
+        startActivity(Intent.createChooser(shareTrack, trackTitle))*/
 
     }
 
