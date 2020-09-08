1e0bffeb91f925fd4ce7f2e66f53b3c9d5679448
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-09-08 19:57:27 +0200

    default accuracy

--- app/src/main/java/com/gonzalocl/trackingexport/app/TrackingExport.kt
+++ app/src/main/java/com/gonzalocl/trackingexport/app/TrackingExport.kt
@@ -3,6 +3,6 @@ package com.gonzalocl.trackingexport.app
 object TrackingExport {
     var timeInterval: Long = 30*60*1000
     var filterAccuracy = true
-    var filterAccuracyThreshold: Long = 10
+    var filterAccuracyThreshold: Long = 15
     var currentTrackTitle = ""
 }
